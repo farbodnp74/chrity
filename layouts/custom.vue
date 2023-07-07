c6ef3e47c58f975618dd712f9c818d6d063b3390
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-07-07 17:42:13 +0330

    c

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -8,12 +8,12 @@
     </main>
     <div class="bg-opacity-100 bg-gray-900  right-20 bottom-0 z-30 w-50 sticky items-center justify-center self-center my-auto text-center" dir="rtl">
       <div  class="bg-green-200 hover:bg-emerald-200 h-16 w-16 rounded-full items-center self-center justify-center my-auto text-center -translate-y-7 mx-5">
-<!--        <a class="hidden lg:block" target="_blank" href="https://web.whatsapp.com/send?phone=989201206957">-->
-<!--          <img class="" src="~/assets/pic/icon.png"  />-->
-<!--        </a>-->
-<!--        <a class="block lg:hidden" target="_blank" href="https://api.whatsapp.com/send?phone=989201206957&text=سلام%20به%20ایرانی%20مارکت%20">-->
-<!--          <img class="" src="~/assets/pic/icon.png"  />-->
-<!--        </a>-->
+        <a class="hidden lg:block" target="_blank" href="https://web.whatsapp.com/send?phone=989201206957">
+          <img class="" src="~/assets/icon.jpg"  />
+        </a>
+        <a class="block lg:hidden" target="_blank" href="https://api.whatsapp.com/send?phone=989201206957&text=سلام%20به%20ایرانی%20مارکت%20">
+          <img class="" src="~/assets/icon.jpg"  />
+        </a>
       </div>
     </div>
     <TheFooter/>

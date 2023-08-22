bda487d30390486f38bbb78c70cb0ac6109445b5
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-08-22 13:31:48 +0330

    k

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -6,7 +6,7 @@
     <main class="relative">
         <slot/>
     </main>
-    <div class="  right-20 bottom-0 z-30 w-50 sticky items-center justify-center self-center my-auto text-center" dir="rtl">
+    <div class="bg-gray-900 bg-opacity-100 p-3  right-20 bottom-0 z-30 w-50 items-center justify-center self-center my-auto text-center" dir="rtl">
       <div  class="bg-gray-200   h-32 w-32 rounded-full items-center self-center justify-center my-auto text-center" style="padding: 20px!important;">
         <a class="" referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=366374&amp;Code=dDJh9IlxlqgArmB8bvfH"><img referrerpolicy="origin" src="https://trustseal.enamad.ir/logo.aspx?id=366374&amp;Code=dDJh9IlxlqgArmB8bvfH" alt="" style="cursor:pointer" id="dDJh9IlxlqgArmB8bvfH"></a>
 

9e7b142bb421cfb11eadf3330a5cd217b2eb08a0
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-08-22 12:58:19 +0330

    k

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -7,13 +7,9 @@
         <slot/>
     </main>
     <div class="bg-opacity-100  bg-gray-900  right-20 bottom-0 z-30 w-50 sticky items-center justify-center self-center my-auto text-center" dir="rtl">
-      <div  class="bg-green-200  hover:bg-emerald-200 h-16 w-16 rounded-full items-center self-center justify-center my-auto text-center -translate-y-7 mx-5">
-        <a class="hidden rounded-full lg:block" target="_blank" href="https://web.whatsapp.com/send?phone=989367942426">
-          <img class="rounded-full" src="~/assets/prod/7.jpg"  />
-        </a>
-        <a class="block rounded-full lg:hidden" target="_blank" href="https://api.whatsapp.com/send?phone=989367942426">
-          <img class="rounded-full" src="~/assets/prod/7.jpg"  />
-        </a>
+      <div  class="bg-green-200 rounded-full  hover:bg-emerald-200 h-16 w-16 rounded-full items-center self-center justify-center my-auto text-center -translate-y-7 mx-5">
+        <a class="rounded-full" referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=366374&amp;Code=dDJh9IlxlqgArmB8bvfH"><img referrerpolicy="origin" src="https://trustseal.enamad.ir/logo.aspx?id=366374&amp;Code=dDJh9IlxlqgArmB8bvfH" alt="" style="cursor:pointer" id="dDJh9IlxlqgArmB8bvfH"></a>
+
       </div>
     </div>
     <TheFooter/>
